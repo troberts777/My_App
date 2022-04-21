eacbae977a147d30e005face215b046f3a729e50
TROBERTS <troberts777@mail.com>
2022-04-20 18:58:23 -0700

    Firebase Added, user can signup

--- app/src/main/java/com/example/my_app/MainActivity.java
+++ app/src/main/java/com/example/my_app/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.my_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.my_app.signup.SignupActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText etEmail, etPassword;
+    private String email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
     }
+
+    public void btnSignUpClick(View v)
+    {
+        startActivity(new Intent(this, SignupActivity.class));
+
+    }
+
+    public void btnLoginClick(View v)
+    {
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        if(email.equals(""))
+        {
+            etEmail.setError(getString(R.string.enter_email));
+        }
+        else if (password.equals(""))
+        {
+            etPassword.setError(getString(R.string.enter_password));
+        }
+        else
+        {
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        //startActivity(new Intent(this, HomeActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity. this, "Login Failed : " +
+                                task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
